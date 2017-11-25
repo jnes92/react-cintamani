9631bda937ef41db3100357e8de6342407ad0beb
Jonas PC WIN <jonas.oesterle@gmx.de>
2017-11-25 13:34:48 +0100

    #44: Added routes dynamically for each product

--- cintamani-next/data/routesHelper.js
+++ cintamani-next/data/routesHelper.js
@@ -15,10 +15,13 @@ class RoutesHelper {
     const importedProducts = ExcelImporter.import();
     const flatCategories = ExcelImporter.getFlatCategories(importedProducts);
 
+    // Get Home, Static Routes: index.js
     let routes = {
       '/': { page: '/' },
+      /// TODO: Add Static Routes like impressum, etc.
     }
 
+    // Get All Routes for category.js (ProductLists)
     flatCategories.forEach((flatCategory) => {
       let main = _.trim(flatCategory.split("-")[0]);
       let side = _.trim(flatCategory.split("-")[1]);
@@ -28,6 +31,19 @@ class RoutesHelper {
         query: { main, side }
       }
     });
+
+    // GetAllRoutes for product.js (All Products Directlink)
+    importedProducts.forEach((product) => {
+      let main = _.trim(product[cellNames.Category].split("-")[0]);
+      let side = _.trim(product[cellNames.Category].split("-")[1]);
+      let id = product[cellNames.ID];
+      let pathName = "/" + main + "/" + side + "/" + id;
+      routes[pathName] = {
+        page: '/product',
+        query: { id }
+      }
+    });
+
     return routes;
   }
 
