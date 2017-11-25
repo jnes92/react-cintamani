6e5a7995c6eff466d4f34de32614e46d2bf9b6ec
Jonas PC WIN <jonas.oesterle@gmx.de>
2017-11-25 23:45:12 +0100

    Test with call()

--- cintamani-next/deploy/create-slot.ps1
+++ cintamani-next/deploy/create-slot.ps1
@@ -7,10 +7,11 @@ Param
     [Parameter (Mandatory = $false)]
     [string]$resGroup="cintamani-buddhas",
     [Parameter (Mandatory = $false)]    
-    [string]$env = "develop-"
+    [string]$env = "develop"
 )
 
 $slot=$env+"-"+$id;
-
-az webapp deployment slot create --name $appName --resource-group $resGroup --slot $slot
+# cintamani-buddhas-develop--test.azurewebsites.net
+Write-Output("Setup new slot now: http://" + $appName + "-" + $env +"-"+$id+".azurewebistes.net");
+call az webapp deployment slot create --name $appName --resource-group $resGroup --slot $slot
 Write-Output("Please remember to delete slot, after testing");
